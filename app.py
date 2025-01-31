217848fef5eb6280d5c866fb0690815b4f43daee
goldgolu <mohammadsalam.11110@gmail.com>
2025-01-30 22:19:35 -0700

    Update app.py
    Signed-off-by: goldgolu <mohammadsalam.11110@gmail.com>

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/menu')
 def menu():
