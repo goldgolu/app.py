fe928684062ab7b1c4987bab7d91fb7c750a31ea
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-01 02:13:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'images/logo.png')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # Database setup
 def init_db():
