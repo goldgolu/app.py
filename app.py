2fb7af8b352206ec33e597080e6f2d08f3b7c8c3
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-02 04:20:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_templates, send_from_directory
+from flask import Flask, render_template, send_from_directory
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from flask import request, redirect, jsonify
