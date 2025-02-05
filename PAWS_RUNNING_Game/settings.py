c305ae0b121c7d007ddd3f21bb8b8c645817edec
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-05 10:14:22 -0700

    Update settings.py

--- PAWS_RUNNING_Game/settings.py
+++ PAWS_RUNNING_Game/settings.py
@@ -1,7 +1,10 @@
 import os
 
-STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
-TEMPLATES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Static aur Templates ka path define karo
+STATIC_FOLDER = os.path.join(BASE_DIR, "static")
+TEMPLATE_FOLDER = os.path.join(BASE_DIR, "templates")
 
 # Flask Configuration
 SECRET_KEY = os.getenv("SECRET_KEY", "your_default_secret_key")
