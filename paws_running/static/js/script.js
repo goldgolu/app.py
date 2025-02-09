f3e0e97427ffb7c72bafa1a9f4f822297931e7ab
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-09 00:45:08 -0700

    Update script.js

--- paws_running/static/js/script.js
+++ paws_running/static/js/script.js
@@ -1,1 +1,14 @@
+// static/js/script.js
 
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Document loaded and ready!");
+
+    // Example: Handling button click for general actions
+    const someButton = document.querySelector('#some-button');
+    if (someButton) {
+        someButton.addEventListener('click', function() {
+            alert("Button clicked!");
+            // Add additional functionality here
+        });
+    }
+});
