90a70b845550c49777c889bbffcd297aaf7f54d3
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-12 08:38:55 -0700

    Create models.py

---
+++ paws_running/database/models.py
@@ -0,0 +1,15 @@
+# database/models.py
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    coins = db.Column(db.Integer, default=0)
+    level = db.Column(db.Integer, default=1)
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
