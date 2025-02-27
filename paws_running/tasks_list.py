2a77f9ae7d793b6d004f55779cd9e232ecc16edb
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-27 02:30:11 -0700

    Create tasks_list.py

---
+++ paws_running/tasks_list.py
@@ -0,0 +1,8 @@
+def add_task(task_name):
+    return f"Task {task_name} added successfully!"
+
+def complete_task(task_id):
+    return f"Task {task_id} completed!"
+
+def get_tasks():
+    return ["task1", "task2", "task3"]
