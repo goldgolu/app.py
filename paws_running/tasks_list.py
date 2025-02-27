4d4becebe4e10c2c269c930fab4f21350384dc58
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-27 02:48:15 -0700

    Delete paws_running/tasks_list.py

--- paws_running/tasks_list.py
+++
@@ -1,8 +0,0 @@
-def add_task(task_name):
-    return f"Task {task_name} added successfully!"
-
-def complete_task(task_id):
-    return f"Task {task_id} completed!"
-
-def get_tasks():
-    return ["task1", "task2", "task3"]
