709f7caf3e60c8e2d75670d9f180b861725d69cd
goldgolu <mohammadsalam.11110@gmail.com>
2025-02-27 03:38:46 -0700

    Update celery.py

--- paws_running/celery.py
+++ paws_running/celery.py
@@ -1,8 +1,14 @@
 import os
 from celery import Celery
 
-# Redis URL ko environment variable se lo, aur SSL error fix karo
-REDIS_URL = os.getenv("REDIS_URL", "rediss://<your_redis_url>?ssl_cert_reqs=CERT_NONE")
+# Redis URL ko environment variable se lein aur default value set karein
+REDIS_URL = os.getenv("REDIS_URL")
+if not REDIS_URL:
+    raise ValueError("REDIS_URL environment variable not set!")
+
+# Agar Redis SSL cert error ho to use fix karein
+if REDIS_URL.startswith("rediss://") and "ssl_cert_reqs" not in REDIS_URL:
+    REDIS_URL += "?ssl_cert_reqs=CERT_NONE"
 
 def make_celery():
     celery = Celery(
@@ -11,11 +17,13 @@ def make_celery():
         backend=REDIS_URL
     )
     
-    # Agar extra configurations add karni ho to yaha karein
+    # Celery configurations
     celery.conf.update(
         task_serializer='json',
         accept_content=['json'],
-        result_serializer='json'
+        result_serializer='json',
+        timezone='UTC',
+        enable_utc=True
     )
     
     return celery
